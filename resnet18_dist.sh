f02af0adda4eec60a6b877a420eaa3fce0f37533
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-03-26 21:05:36 +0000

    h

--- resnet18_dist.sh
+++ resnet18_dist.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 for source in {0..9}; do
-  for target in {0..9}; do
-    if [ $source -ne $target ]; then
-      echo "Running source=$source, target=$target"
-      python3 resnet18_dist.py --source $source --target_tasks $target --num_samples 3000 --num_projections 100000
-    fi
+  for target in $(seq $((source + 1)) 9); do
+    echo "Running source=$source, target=$target"
+    python3 resnet18_dist.py --source $source --target_tasks $target --num_samples 3000 --num_projections 100000
   done
 done
+
