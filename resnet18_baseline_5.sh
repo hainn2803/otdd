fa58c0e23813880ef3dcacfd7d5bb33371905b0d
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-03-28 12:39:28 +0000

    h

--- resnet18_baseline_5.sh
+++ resnet18_baseline_5.sh
@@ -19,4 +19,4 @@ eval "$(conda shell.bash hook)"
 conda activate /lustre/scratch/client/movian/research/users/hainn14/envs/otdd
 cd /lustre/scratch/client/movian/research/users/hainn14/otdd
 
-python3 resnet18_baseline.py --num_epochs 20 --parent_dir saved_split_task_10 --learning_rate 0.01 --source_task 5
+python3 resnet18_baseline.py --num_epochs 20 --parent_dir saved_split_task_10 --learning_rate 0.01 --task_num 5
