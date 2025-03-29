d3d961eee4003eba731e9c2678f8e147f4f26f32
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-03-29 10:53:45 +0000

    h

--- resnet18_dist_otdd.sh
+++ resnet18_dist_otdd.sh
@@ -3,7 +3,7 @@
 for source in {0..9}; do
   for target in $(seq $((source + 1)) 9); do
     echo "Running source=$source, target=$target"
-    python3 resnet18_dist_otdd.py --source $source --target_tasks $target --num_samples 400 --parent_dir saved_split_task_10
+    python3 resnet18_dist_otdd.py --source $source --target_tasks $target --num_samples 2000 --parent_dir saved_split_task_10
   done
 done
 
