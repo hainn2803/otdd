152021216e8be6da99988bdf8ce1c1c7720856fc
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-04-01 04:17:43 +0000

    h

--- resnet18_dist.py
+++ resnet18_dist.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 from torch.utils.data import DataLoader, TensorDataset
 from otdd.pytorch.method5 import compute_pairwise_distance
+import gc
 
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
